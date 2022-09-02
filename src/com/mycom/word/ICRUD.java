49dd5c5220d4635ac1688cf90b8d9089fac9bef2
Gyeongho <22100715@handong.ac.kr>
2022-09-02 10:50:05 +0900

    project 1_1

--- src/com/mycom/word/ICRUD.java
+++ src/com/mycom/word/ICRUD.java
@@ -2,8 +2,11 @@ package com.mycom.word;
 
 public interface ICRUD {
 	public Object add();
+
 	public int update(Object obj);
+
 	public int delete(Object obj);
+
 	public void selectOne(int id);
 
 }
--- src/com/mycom/word/WordManager.java
+++ src/com/mycom/word/WordManager.java
@@ -5,49 +5,36 @@ import java.util.Scanner;
 public class WordManager {
 	Scanner s = new Scanner(System.in);
 	WordCRUD wordCRUD;
+
 	/*
 	 *** 영단어 마스터 ***
 	 ********************
-	 1. 모든 단어 보기
-	 2. 수준별 단어 보기
-	 3. 단어 검색
-	 4. 단어 추가
-	 5. 단어 수정
-	 6. 단어 삭제
-	 7. 파일 저장
-	 0. 나가기
+	 * 1. 모든 단어 보기 2. 수준별 단어 보기 3. 단어 검색 4. 단어 추가 5. 단어 수정 6. 단어 삭제 7. 파일 저장 0. 나가기
 	 ********************
-	 => 원하는 메뉴는? 4
+	 * => 원하는 메뉴는? 4
 	 */
-	WordManager(){
+	WordManager() {
 		wordCRUD = new WordCRUD(s);
 	}
 
 	public int selectMenu() {
-		System.out.println("*** 영단어 마스터 ***\r\n"
-				+ "********************\r\n"
-				+ "1. 모든 단어 보기\r\n"
-				+ "2. 수준별 단어 보기\r\n"
-				+ "3. 단어 검색\r\n"
-				+ "4. 단어 추가\r\n"
-				+ "5. 단어 수정\r\n"
-				+ "6. 단어 삭제\r\n"
-				+ "7. 파일 저장\r\n"
-				+ "0. 나가기\r\n"
-				+ "********************\r\n"
-				+ "=> 원하는 메뉴는? ");
-		
+		System.out.println("*** 영단어 마스터 ***\r\n" + "********************\r\n" + "1. 모든 단어 보기\r\n" + "2. 수준별 단어 보기\r\n"
+				+ "3. 단어 검색\r\n" + "4. 단어 추가\r\n" + "5. 단어 수정\r\n" + "6. 단어 삭제\r\n" + "7. 파일 저장\r\n" + "0. 나가기\r\n"
+				+ "********************\r\n" + "=> 원하는 메뉴는? ");
+
 		return s.nextInt();
 	}
 
 	public void start() {
-		while(true) {
+		while (true) {
 			int menu = selectMenu();
-			if(menu == 0) break;
-			if(menu == 4) {
-				wordCRUD.addWord();
+			if (menu == 0) {
+				System.out.println("프로그램종료!다음에 만나요~");
+				break;
 			}
-			else if(menu == 1) {
+			if (menu == 4) {
+				wordCRUD.addWord();
+			} else if (menu == 1) {
 				wordCRUD.listAll();
 			}
 		}
